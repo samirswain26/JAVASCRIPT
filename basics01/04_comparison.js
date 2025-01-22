699898fb0f223b9e13e0d677b129f3e974d0a2c7
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-01-22 10:19:36 +0530

    Learning comparison and logical operators.

---
+++ basics01/04_comparison.js
@@ -0,0 +1,17 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log("2" === 2);
+
+// Always try to make the code clean and this type of script is not prefferable to erite.
