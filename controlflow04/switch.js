6fbb78b08ede598670cab2ad61c55e6ce983e921
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-02-09 22:55:59 +0530

    Learning if-else and other statements.

---
+++ controlflow04/switch.js
@@ -0,0 +1,51 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+let month = 3
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Febraury");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("DEfault month is selected.");
+        
+        break;
+}
+
+let mon = "march"
+switch (mon) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("Febraury");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+
+    default:
+        console.log("DEfault month is selected.");
+        
+        break;
+}
